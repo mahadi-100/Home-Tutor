8f68e5c4966bcf77e65f321b5ca0697cc1aae797
mahadi-100 <mahadihasan10030@gmail.com>
2021-04-11 22:40:30 +0600

    commit

--- app/src/main/java/com/example/hometutor/fragments/PostFragment.java
+++ app/src/main/java/com/example/hometutor/fragments/PostFragment.java
@@ -15,7 +15,9 @@ public class PostFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+
+
         return inflater.inflate(R.layout.fragment_post, container, false);
     }
 }
